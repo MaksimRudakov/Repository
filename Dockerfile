7961bbe4c13e58caf0c677a9a6d606b5d033c4fd
Maxim Rudakov <maxim.rudakov.u@gmail.com>
2019-08-16 17:58:49 +0600

    Пытаюсь вывести весь список репозиториев

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:latest
+FROM python:2.7-alpine3.10 
 
 WORKDIR /
 
--- get_all_repo.py
+++ get_all_repo.py
@@ -8,6 +8,8 @@ import os
 import yaml
 import docker
 import sys
+import requests
+import json
 from docker_registry_client import DockerRegistryClient
 from requests import HTTPError
 
@@ -75,59 +77,58 @@ if __name__ == '__main__':
 
     # Берем url реестров для синхронизации
     src_registry_url = config['source_registry']['url']
-    dst_registry_url = config['destination_registry']['url']
 
 
     # Получаем логин и пароль из конфига
     # ----------------------------------
     src_username = None if 'username' not in config['source_registry'] else str(config['source_registry']['username'])
     src_password = determine_password(config, 'src')
-
-    dst_username = None if 'username' not in config['destination_registry'] else str(config['destination_registry']['username'])
-    dst_password = determine_password(config, 'dst')
     # ----------------------------------
 
     # Регистрация в реестрах 
     src_client = DockerRegistryClient(src_registry_url, username=src_username, password=src_password)
-    dst_client = DockerRegistryClient(dst_registry_url, username=dst_username, password=dst_password)
 
     # Создаем экземпляр клиента
     docker_client = docker.from_env()
 
     # Авторизация
     docker_client.login(registry=src_registry_url, username=src_username, password=src_password)
-    docker_client.login(registry=dst_registry_url, username=dst_username,password=dst_password)
     
     # Берем список контейнеров для синхронизации
     repositories = config['repositories']
 
-    print("Все репы: "+str(src_client.repository()))
+    url = src_registry_url + "/v2/_catalog"
+    print("URL: "+url)
+    r = requests.get(url)
 
-    # Вытаскиваем из реесторов теги, согласно списку из конфига
-    src_tags = get_tags(src_client, repositories, config)
-    dst_tags = get_tags(dst_client, repositories, config)
+    j = json.dumps(r.json())
+    print(j)
 
-    # Выясняем каких тегов нет в dst реестре
-    missing_tags = src_tags - dst_tags
+    # # Вытаскиваем из реесторов теги, согласно списку из конфига
+    # src_tags = get_tags(src_client, repositories, config)
+    # dst_tags = get_tags(dst_client, repositories, config)
 
-    # Цикл по недостающим тегам
-    for missing_tag in missing_tags:
-        # Создаем теги с указателями на реестры
-        src_tag = strip_scheme(src_registry_url) + '/' + missing_tag
-        dst_tag = strip_scheme(dst_registry_url) + '/' + missing_tag
+    # # Выясняем каких тегов нет в dst реестре
+    # missing_tags = src_tags - dst_tags
 
-        # Пулим контейнера из src реестра
-        # sys.stdout.write("Pulling:", src_tag)
-        print("Pulling: "+ src_tag)
-        docker_client.images.pull(src_tag)
+    # # Цикл по недостающим тегам
+    # for missing_tag in missing_tags:
+    #     # Создаем теги с указателями на реестры
+    #     src_tag = strip_scheme(src_registry_url) + '/' + missing_tag
+    #     dst_tag = strip_scheme(dst_registry_url) + '/' + missing_tag
 
-        # Берем загруженный docker контейнер и меняем ему тег 
-        # sys.stdout.write("Changing %s to %s" % (src_tag, dst_tag,))
-        print("Changing %s to %s" % (src_tag, dst_tag,))
-        src_image = docker_client.images.get(name=src_tag)
-        src_image.tag(dst_tag)
+    #     # Пулим контейнера из src реестра
+    #     # sys.stdout.write("Pulling:", src_tag)
+    #     print("Pulling: "+ src_tag)
+    #     docker_client.images.pull(src_tag)
 
-        # Пуш в dst реестр
-        # sys.stdout.write("Pushing:", dst_tag)
-        print("Pushing: "+ dst_tag)
-        docker_client.images.push(dst_tag)
+    #     # Берем загруженный docker контейнер и меняем ему тег 
+    #     # sys.stdout.write("Changing %s to %s" % (src_tag, dst_tag,))
+    #     print("Changing %s to %s" % (src_tag, dst_tag,))
+    #     src_image = docker_client.images.get(name=src_tag)
+    #     src_image.tag(dst_tag)
+
+    #     # Пуш в dst реестр
+    #     # sys.stdout.write("Pushing:", dst_tag)
+    #     print("Pushing: "+ dst_tag)
+    #     docker_client.images.push(dst_tag)
