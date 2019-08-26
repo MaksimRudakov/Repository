92ded69b4850e9875a0cdafdc472cef50c8c1362
Maxim Rudakov <maxim.rudakov.u@gmail.com>
2019-08-26 18:02:24 +0600

    Начал делать удаление

--- docker_registries_sync.py
+++ docker_registries_sync.py
@@ -73,6 +73,7 @@ def determine_password(config, kind):
 # Проверка на основную программу
 if __name__ == '__main__':
 
+
     # Загрузка конфига
     config = load_config()
 
@@ -100,44 +101,47 @@ if __name__ == '__main__':
     # Авторизация
     docker_client.login(registry=src_registry_url, username=src_username, password=src_password)
     docker_client.login(registry=dst_registry_url, username=dst_username,password=dst_password)
-    
-    # Если включена синхронизация всех репозиториев
-    if config['take_all_repositories']:
-        # Получаем список репозиториев
-        # ----------------------------------
-        src_list_repo = requests.get(src_registry_url + "/v2/_catalog").json()
-        dst_list_repo = requests.get(dst_registry_url + "/v2/_catalog").json()
-        # ----------------------------------
-        
-        # Получаем теги по списку
-        src_tags = get_tags(src_client, src_list_repo['repositories'], config)
-        dst_tags = get_tags(dst_client, dst_list_repo['repositories'], config)
+
+    if config['delete_tags']:
+        print('delete_tags')
     else:
-        # Берем список контейнеров для синхронизации
-        repositories = config['repositories']
-        # Вытаскиваем из реесторов теги, согласно списку из конфига
-        src_tags = get_tags(src_client, repositories, config)
-        dst_tags = get_tags(dst_client, repositories, config)
+        # Если включена синхронизация всех репозиториев
+        if config['take_all_repositories']:
+            # Получаем список репозиториев
+            # ----------------------------------
+            src_list_repo = requests.get(src_registry_url + "/v2/_catalog").json()
+            dst_list_repo = requests.get(dst_registry_url + "/v2/_catalog").json()
+            # ----------------------------------
+            
+            # Получаем теги по списку
+            src_tags = get_tags(src_client, src_list_repo['repositories'], config)
+            dst_tags = get_tags(dst_client, dst_list_repo['repositories'], config)
+        else:
+            # Берем список контейнеров для синхронизации
+            repositories = config['repositories']
+            # Вытаскиваем из реесторов теги, согласно списку из конфига
+            src_tags = get_tags(src_client, repositories, config)
+            dst_tags = get_tags(dst_client, repositories, config)
 
 
-    # Выясняем каких тегов нет в dst реестре
-    missing_tags = src_tags - dst_tags
+        # Выясняем каких тегов нет в dst реестре
+        missing_tags = src_tags - dst_tags
 
-    # Цикл по недостающим тегам
-    for missing_tag in missing_tags:
-        # Создаем теги с указателями на реестры
-        src_tag = strip_scheme(src_registry_url) + '/' + missing_tag
-        dst_tag = strip_scheme(dst_registry_url) + '/' + missing_tag
+        # Цикл по недостающим тегам
+        for missing_tag in missing_tags:
+            # Создаем теги с указателями на реестры
+            src_tag = strip_scheme(src_registry_url) + '/' + missing_tag
+            dst_tag = strip_scheme(dst_registry_url) + '/' + missing_tag
 
-        # Пулим контейнера из src реестра
-        print("Pulling: "+ src_tag)
-        docker_client.images.pull(src_tag)
+            # Пулим контейнера из src реестра
+            print("Pulling: "+ src_tag)
+            docker_client.images.pull(src_tag)
 
-        # Берем загруженный docker контейнер и меняем ему тег 
-        print("Changing %s to %s" % (src_tag, dst_tag,))
-        src_image = docker_client.images.get(name=src_tag)
-        src_image.tag(dst_tag)
+            # Берем загруженный docker контейнер и меняем ему тег 
+            print("Changing %s to %s" % (src_tag, dst_tag,))
+            src_image = docker_client.images.get(name=src_tag)
+            src_image.tag(dst_tag)
 
-        # Пуш в dst реестр
-        print("Pushing: "+ dst_tag)
-        docker_client.images.push(dst_tag)
+            # Пуш в dst реестр
+            print("Pushing: "+ dst_tag)
+            docker_client.images.push(dst_tag)
